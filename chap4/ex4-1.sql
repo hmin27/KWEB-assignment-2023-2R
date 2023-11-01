e3ecbfb2b46909d5e52c1e47026376841a2f0dc5
hmin27 <wga0523@naver.com>
2023-11-01 18:09:20 +0900

    4-1, 4-2 commit

--- chap4/ex4-1.sql
+++ chap4/ex4-1.sql
@@ -1,4 +1,4 @@
-// 학교에 소속된 학생들의 데이터를 저장하는 DB 구축
+-- 학교에 소속된 학생들의 데이터를 저장하는 DB 구축
 CREATE TABLE 'students' (
     `id` INT NOT NULL,
     `name` VARCHAR(20) NOT NULL,
@@ -9,6 +9,7 @@ CREATE TABLE 'students' (
     `address` VARCHAR(100) NOT NULL,
     `grade` INT NOT NULL DEFAULT 0,
     `avg_grade` DOUBLE NOT NULL DEFAULT 0.0,
+    -- Default값 설정 
     `attendence` TINYINT(1) NOT NULL DEFAULT 1,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
